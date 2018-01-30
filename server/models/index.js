95aa091d34b66f27eda4df0b710f36caaf9fb77d
johanna fulghum <johanna.fulghum@gmail.com>
2018-01-30 18:00:21 -0500

    Setup models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,79 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize('postgres://localhost:5432/tripdb', {logging: false})
 
-db.define 
+const Place=db.define('place',{
+    address:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    city:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    state:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    phone:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    location:{
+        type: Sequelize.ARRAY(Sequelize.FLOAT),//DataTypes.ARRAY(DataTypes.DECIMAL)
+        allowNull:false
+    }
+
+
+} )
+
+const Hotel=db.define('hotel',{
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    num_stars:{
+        type: Sequelize.FLOAT(1,5),
+        validate:{
+            min:1,
+            max:5
+        }
+        allowNull:false
+    },
+    amenities:{
+        type: Sequelize.STRING,
+        allowNull:false
+    }
+
+} )
+
+const Activity=db.define('activity',{
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    age_range:{
+        type: Sequelize.STRING,
+        allowNull:false
+    }
+
+} )
+
+const Restaurant=db.define('restaurant',{
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    cuisine:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    price:{
+        type: Sequelize.INTEGER,
+        allowNull:false
+    }
+
+} )
+
+Hotel.belongsTo(Place);
+Activity.belongsTo(Place);
+Restaurant.belongsTo(Place);
